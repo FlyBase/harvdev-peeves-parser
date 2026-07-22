86c57eae4f6d06158fc104abbd86e97d8920e57d
Gillian Millburn <gm119@cam.ac.uk>
2026-07-22 18:08:44 +0100

    v.2.93: Changes for obsoleting GA31 field (FTA-208)

--- production/allele.pl
+++ production/allele.pl
@@ -263,11 +263,6 @@ FIELD:
 	    check_dups ($file, $2, $field, \%proforma_fields, \%dup_proforma_fields, $primary_symbol_list, $can_dup{$2} ? 1 : 0);
 		@GA2c_list = process_field_data ($file, $hash_entries, $1, '0', $2, $3, \%proforma_fields, '1');
 	}
-	elsif ($field =~ /^(.*?) (GA31)\..*? :(.*)/s)
-	{
-	    check_dups ($file, $2, $field, \%proforma_fields, \%dup_proforma_fields, $primary_symbol_list, $can_dup{$2} ? 1 : 0);
-	    process_field_data ($file, $hash_entries, $1, '1', $2, $3, \%proforma_fields, '0');
-	}
 	elsif ($field =~ /^(.*?) (GA32a)\..*? :(.*)/s)
 	{
 	    check_dups ($file, $2, $field, \%proforma_fields, \%dup_proforma_fields, $primary_symbol_list, $can_dup{$2} ? 1 : 0);
